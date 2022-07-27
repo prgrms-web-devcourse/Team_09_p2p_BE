f9c5ce25529948111a88c2faf656776c570b62f8
Hyunggeun447 <bopi447@naver.com>
2022-07-27 18:52:59 +0900

    [p2p-141] feature: base entity 구성

---
+++ src/main/java/com/prgrms/p2p/domain/common/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.prgrms.p2p.domain.common;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(value = {AuditingEntityListener.class})
+public abstract class BaseEntity implements Serializable {
+
+  @CreationTimestamp
+  @Column(name = "created_at", updatable = false)
+  private LocalDateTime createdAt;
+
+  @UpdateTimestamp
+  @Column(name = "updated_at", updatable = true)
+  private LocalDateTime updatedAt;
+}
