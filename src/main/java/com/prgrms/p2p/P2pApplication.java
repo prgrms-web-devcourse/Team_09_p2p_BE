2e7215160f752c15b9511f6689dcb0b22c45cdba
beomsic <qjatjr29@khu.ac.kr>
2022-07-27 15:09:51 +0900

    setting: 초기 설정

---
+++ src/main/java/com/prgrms/p2p/P2pApplication.java
@@ -0,0 +1,13 @@
+package com.prgrms.p2p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2pApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(P2pApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/prgrms/p2p/P2pApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prgrms.p2p;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P2pApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
