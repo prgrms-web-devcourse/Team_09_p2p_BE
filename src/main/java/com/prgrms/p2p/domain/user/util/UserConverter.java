bd56c29aead7616495bb3a3216f3c470b911e751
beomsic <qjatjr29@khu.ac.kr>
2022-07-29 18:15:42 +0900

    [P2P-22] feat: user 상세조회 dto 기능 추가

--- src/main/java/com/prgrms/p2p/domain/user/util/UserConverter.java
+++ src/main/java/com/prgrms/p2p/domain/user/util/UserConverter.java
@@ -1,7 +1,10 @@
 package com.prgrms.p2p.domain.user.util;
 
 import com.prgrms.p2p.domain.user.dto.SignUpRequest;
+import com.prgrms.p2p.domain.user.dto.UserDetailResponse;
 import com.prgrms.p2p.domain.user.entity.User;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class UserConverter {
 
@@ -14,4 +17,30 @@ public class UserConverter {
             signUpRequest.getSex()
         );
   }
+
+  public static UserDetailResponse detailFromUser(User user) {
+
+    String profileUrl = user.getProfileUrl()
+        .orElse(null);
+
+    return UserDetailResponse.builder()
+        .id(user.getId())
+        .email(user.getEmail())
+        .nickname(user.getNickname())
+        .profileImage(profileUrl)
+        .birth(fromLocalDate(user.getBirth()))
+        .sex(user.getSex())
+        .createdAt(fromLocalDateTime(user.getCreatedAt()))
+        .updatedAt(fromLocalDateTime(user.getUpdatedAt()))
+        .build();
+  }
+
+  private static String fromLocalDate(LocalDate date) {
+    return date.toString();
+  }
+
+  private static String fromLocalDateTime(LocalDateTime date) {
+    return date.toString();
+  }
+
 }
