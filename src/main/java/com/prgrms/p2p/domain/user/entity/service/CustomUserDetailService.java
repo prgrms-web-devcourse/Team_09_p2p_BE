572c8eac733c4b592a3f2d072a725223e0d6f3d8
unnokid <kimki0609@gmail.com>
2022-07-29 16:11:19 +0900

    [P2P-20] feat: CustomUserDetailService 생성

---
+++ src/main/java/com/prgrms/p2p/domain/user/entity/service/CustomUserDetailService.java
@@ -0,0 +1,22 @@
+package com.prgrms.p2p.domain.user.entity.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+  private final UserRepository userRepository;
+
+  public CustomUserDetailService(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    return userRepository.findByEmail(username)
+        .orElseThrow(() -> new IllegalArgumentException("아이디를 찾을 수 없습니다."));
+  }
+}
