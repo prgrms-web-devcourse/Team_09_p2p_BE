549a699234b0c969e6ec83b19e1b10bc83873d75
beomsic <qjatjr29@khu.ac.kr>
2022-07-29 17:45:16 +0900

    [P2P-28] feat: User 생성자, get, set 기능 추가

--- src/main/java/com/prgrms/p2p/domain/user/entity/User.java
+++ src/main/java/com/prgrms/p2p/domain/user/entity/User.java
@@ -1,9 +1,12 @@
 package com.prgrms.p2p.domain.user.entity;
 
+import static org.apache.logging.log4j.util.Strings.isBlank;
+
 import com.prgrms.p2p.domain.comment.entity.Comment;
 import com.prgrms.p2p.domain.common.BaseEntity;
 import com.prgrms.p2p.domain.course.entity.Course;
 import com.prgrms.p2p.domain.place.entity.Place;
+import com.prgrms.p2p.domain.user.util.PasswordEncrypter;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -63,5 +66,70 @@ public class User extends BaseEntity {
 
   public void addCourse(Course course) {
     this.courses.add(course);
+  }
+
+  public User(String email, String password, String nickname, String birth, Sex sex) {
+    setEmail(email);
+    setPassword(password);
+    setNickname(nickname);
+    setBirth(birth);
+    setSex(sex);
+  }
+
+  public void matchPassword(String password) {
+    //TODO: 예외 바꿔주기
+    String encrypted = PasswordEncrypter.encrypt(password);
+    if(!PasswordEncrypter.isMatch(encrypted, this.password)) {
+      throw new IllegalArgumentException();
+    }
+  }
+
+  public void changePassword(String newPassword) {
+    setPassword(newPassword);
+  }
+
+  public void changeNickname(String nickname) {
+    setNickname(nickname);
+  }
+
+  public void changeBirth(String birth) { setBirth(birth); }
+
+  public void changeSex(Sex Sex) {
+    setSex(sex);
+  }
+
+  private void checkBlank(String target) {
+    //TODO: InvalidParamException
+    if(isBlank(target)) {
+      throw new RuntimeException();
+    }
+  }
+
+  private void setEmail(String email) {
+    checkBlank(email);
+    this.email = email;
+  }
+
+  private void setPassword(String password) {
+    checkBlank(password);
+    this.password = password;
+  }
+
+  private void setNickname(String nickname) {
+    checkBlank(nickname);
+    this.nickname = nickname;
+  }
+
+  private void setBirth(String birth) {
+    checkBlank(birth);
+    this.birth = LocalDate.parse(birth);
+  }
+  private void setSex(Sex sex) {
+    this.sex = sex;
+  }
+
+  private void setProfileUrl(String profileUrl) {
+    checkBlank(profileUrl);
+    this.profileUrl = profileUrl;
   }
 }
