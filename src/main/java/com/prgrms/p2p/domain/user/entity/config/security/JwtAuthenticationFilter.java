ad0025e240440d9d7cacead3ef424d71bfb06395
unnokid <kimki0609@gmail.com>
2022-07-29 16:13:38 +0900

    [P2P-20] feat: doFilter 부분 수정

--- src/main/java/com/prgrms/p2p/domain/user/entity/config/security/JwtAuthenticationFilter.java
+++ src/main/java/com/prgrms/p2p/domain/user/entity/config/security/JwtAuthenticationFilter.java
@@ -7,6 +7,8 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
 
 @RequiredArgsConstructor
@@ -21,7 +23,9 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
     String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
 
     if(token != null && jwtTokenProvider.validateToken(token)){
-
+      Authentication authentication = jwtTokenProvider.getAuthentication(token);
+      SecurityContextHolder.getContext().setAuthentication(authentication);
     }
+    chain.doFilter(request,response);
   }
 }
