5a18cda60ad0e97cba06eb023ff1f2924d8abd6d
송무송 (Kate) <real.purple.hae.s@gmail.com>
2022-08-01 00:10:56 +0900

    [P2P-108] test: PlaceLike 엔티티 생성 로직 검증 테스트 추가

---
+++ src/test/java/com/prgrms/p2p/domain/like/entity/PlaceLikeTest.java
@@ -0,0 +1,56 @@
+package com.prgrms.p2p.domain.like.entity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
+
+import com.prgrms.p2p.domain.place.entity.Place;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.DisplayName.class)
+class PlaceLikeTest {
+
+  @Test
+  @DisplayName("성공 : 장소 좋아요 객체를 생성합니다.")
+  public void success() {
+    // Given
+    Place place = new Place("1234", "1234", null, "1234", "1234", null, null, null);
+    // When
+    PlaceLike placeLike = new PlaceLike(1L, place);
+    // Then
+    assertThat(place.getPlaceLikes()).contains(placeLike);
+  }
+
+  @Test
+  @DisplayName("실패 : 유저 아이디가 빈값인 경우 예외를 반환합니다.")
+  public void failByUserId1() {
+    // Given
+    Place place = new Place("1234", "1234", null, "1234", "1234", null, null, null);
+    // When
+    Throwable response = catchThrowable(() -> new PlaceLike(null, place));
+    // Then
+    assertThat(response).isInstanceOf(RuntimeException.class);
+  }
+
+  @Test
+  @DisplayName("실패 : 유저 아이디가 0 이하인 경우 예외를 반환합니다.")
+  public void failByUserId2() {
+    // Given
+    Place place = new Place("1234", "1234", null, "1234", "1234", null, null, null);
+    // When
+    Throwable response = catchThrowable(() -> new PlaceLike(0L, place));
+    // Then
+    assertThat(response).isInstanceOf(RuntimeException.class);
+  }
+
+  @Test
+  @DisplayName("실패 : 장소가 빈값인 경우 예외를 반환합니다.")
+  public void failByPlace() {
+    // When
+    Throwable response = catchThrowable(() -> new PlaceLike(1L, null));
+    // Then
+    assertThat(response).isInstanceOf(RuntimeException.class);
+  }
+}
