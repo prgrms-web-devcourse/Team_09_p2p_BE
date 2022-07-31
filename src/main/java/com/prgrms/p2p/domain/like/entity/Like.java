68c70026e3b0342ff41321dc0af1812ef71fb2b8
송무송 (Kate) <real.purple.hae.s@gmail.com>
2022-08-01 00:43:12 +0900

    [P2P-108] fix: Context 업로드 오류 해결

--- src/main/java/com/prgrms/p2p/domain/like/entity/Like.java
+++ src/main/java/com/prgrms/p2p/domain/like/entity/Like.java
@@ -2,12 +2,14 @@ package com.prgrms.p2p.domain.like.entity;
 
 import com.prgrms.p2p.domain.common.BaseEntity;
 import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.util.ObjectUtils;
 
 @Getter
+@MappedSuperclass
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public abstract class Like extends BaseEntity {
 
