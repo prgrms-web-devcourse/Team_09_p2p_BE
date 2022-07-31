e7b359284c32dd3243d47b93fce5fd4f695f2f47
beomsic <qjatjr29@khu.ac.kr>
2022-07-31 16:37:40 +0900

    [P2P-28] feature: 닉네임 정규표현식 추가 및 패스워드, 이메일 정규표현식 수정

--- src/main/java/com/prgrms/p2p/domain/user/util/Validation.java
+++ src/main/java/com/prgrms/p2p/domain/user/util/Validation.java
@@ -2,22 +2,33 @@ package com.prgrms.p2p.domain.user.util;
 
 public class Validation {
 
-  public static void validatePassword(String password) {
-    String pattern = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{12,}$";
+  public static boolean validatePassword(String password) {
+    String pattern = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d~!@#$%^&*()+|=]{8,15}$";
 
     // TODO: exception 다시 만들기 - InvalidPasswordException
-    if(!pattern.matches(password)) {
+    if(!password.matches(pattern)) {
       throw new IllegalArgumentException();
     }
+    return true;
   }
 
-  public static void validateEmail(String email) {
+  public static boolean validateEmail(String email) {
     String pattern = "^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$";
 
     // TODO: exception 다시 만들기 - InvalidEmailException
-    if(!pattern.matches(email)) {
+    if(!email.matches(pattern)) {
       throw new IllegalArgumentException();
     }
+    return true;
   }
 
+  public static boolean validateNickname(String nickname) {
+    String pattern = "^.{3,6}$";
+
+    // TODO: exception 다시 만들기 - InvalidNicknameException
+    if(!nickname.matches(pattern)) {
+      throw new IllegalArgumentException();
+    }
+    return true;
+  }
 }
