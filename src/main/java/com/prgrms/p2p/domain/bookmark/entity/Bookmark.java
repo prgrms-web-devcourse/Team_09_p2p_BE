8a8d3f0bf1c58a357375550232020a0658f76fa0
송무송 (Kate) <real.purple.hae.s@gmail.com>
2022-08-01 17:18:14 +0900

    [P2P-110] fix: Bookmark 상위 객체 오류 해결

--- src/main/java/com/prgrms/p2p/domain/bookmark/entity/Bookmark.java
+++ src/main/java/com/prgrms/p2p/domain/bookmark/entity/Bookmark.java
@@ -1,31 +1,29 @@
 package com.prgrms.p2p.domain.bookmark.entity;
 
 import com.prgrms.p2p.domain.common.BaseEntity;
+import java.util.Objects;
 import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.Where;
 
-
-@Where(clause = "is_deleted = false")
-@SQLDelete(sql = "UPDATE room SET is_deleted = true WHERE id = ?")
 @Getter
+@MappedSuperclass
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public abstract class Bookmark extends BaseEntity {
 
   @Column(name = "user_id")
   private Long userId;
 
-  @Column(name = "is_deleted")
-  private Boolean isDeleted = Boolean.FALSE;
-
   public Bookmark(Long userId) {
-    this.userId = userId;
+    setUserId(userId);
   }
 
   private void setUserId(Long userId) {
+    if (Objects.isNull(userId) || userId <= 0) {
+      throw new IllegalArgumentException("잘못된 사용자 아이디가 입력되었습니다.");
+    }
     this.userId = userId;
   }
 }
