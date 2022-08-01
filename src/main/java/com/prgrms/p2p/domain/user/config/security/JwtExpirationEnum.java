10c23fcb30be8383c714eb47c26ed70a60a348a3
unnokid <unnokid@gmail.com>
2022-08-01 16:15:48 +0900

    [P2P-20] feat: Enum 타입 추가

--- src/main/java/com/prgrms/p2p/domain/user/config/security/JwtExpirationEnum.java
+++ src/main/java/com/prgrms/p2p/domain/user/config/security/JwtExpirationEnum.java
@@ -1,7 +1,9 @@
 package com.prgrms.p2p.domain.user.config.security;
 
 public enum JwtExpirationEnum {
-  ACCESS_TOKEN_EXPIRATION_TIME("AccessToken 만료 시간 / 7일", 1000L * 60 * 60 * 24 * 7);
+  ACCESS_TOKEN_EXPIRATION_TIME("AccessToken 만료 시간 / 7일", 1000L * 60 * 60 * 24 * 7),
+  LOGIN_EXPIRATION_TIME("로그인 횟수시도 만료 시간 / 6시간 ", 1000L * 60 * 60 * 6),
+  BAN_EXPIRATION_TIME("로그인 금지 만료 시간 / 1일", 1000L * 60 * 60 * 24);
 
   private String description;
   private Long value;
