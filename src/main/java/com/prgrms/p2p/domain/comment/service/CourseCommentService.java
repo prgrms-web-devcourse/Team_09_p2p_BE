c03cc8f6a164d6c569aa02f13282067f45ddb21e
Hyunggeun447 <bopi447@naver.com>
2022-08-03 15:43:54 +0900

    [P2P-153] feat: 코스에 대한 댓글을 작성을 테스트합니다.

--- src/main/java/com/prgrms/p2p/domain/comment/service/CourseCommentService.java
+++ src/main/java/com/prgrms/p2p/domain/comment/service/CourseCommentService.java
@@ -6,6 +6,8 @@ import com.prgrms.p2p.domain.comment.repository.CourseCommentRepository;
 import com.prgrms.p2p.domain.comment.util.CommentConverter;
 import com.prgrms.p2p.domain.course.entity.Course;
 import com.prgrms.p2p.domain.course.repository.CourseRepository;
+import com.prgrms.p2p.domain.user.repository.UserRepository;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,24 +19,50 @@ public class CourseCommentService {
 
   private final CourseCommentRepository courseCommentRepository;
   private final CourseRepository courseRepository;
+  private final UserRepository userRepository;
 
-  /**
-   * 반환값은 코스 아이디입니다.
-   */
   @Transactional
   public Long save(CreateCourseCommentRequest createCommentReq) {
 
-    Course course = courseRepository.findById(createCommentReq.getCourseId())
-        .orElseThrow(()->new RuntimeException("없는 게시물입니다."));
+    validateUser(createCommentReq.getUserId());
 
-    Long seq = createCommentReq.getRootCommentId().isEmpty() ?
-        0L : courseCommentRepository.findSequence(
-        createCommentReq.getRootCommentId().get()) + 1;
+    Course course = courseRepository.findById(createCommentReq.getCourseId())
+        .orElseThrow(() -> new RuntimeException("게시물이 존재하지 않습니다."));
+
+    validateRootComment(createCommentReq.getRootCommentId());
+
+    Long seq = getSeq(createCommentReq);
 
     CourseComment courseComment = CommentConverter.toCourseComment(createCommentReq, course, seq);
 
-    courseCommentRepository.save(courseComment);
+    return courseCommentRepository.save(courseComment).getId();
 
-    return createCommentReq.getCourseId();
   }
+
+  private void validateUser(Long userId) {
+    if (!userRepository.existsById(userId)) {
+      throw new RuntimeException("사용자가 존재하지 않습니다.");
+    }
+  }
+
+  private void validateRootComment(Optional<Long> rootCommentId) {
+    rootCommentId.ifPresent(id->{
+      if (!courseCommentRepository.existsById(id)) {
+        throw new RuntimeException("존재하지 않는 댓글에 하위 댓글을 작성할 수 없습니다.");
+      }
+    });
+  }
+
+  private Long getSeq(CreateCourseCommentRequest createCommentReq) {
+    Long seq = createCommentReq.getRootCommentId().isEmpty() ?
+        0L : courseCommentRepository.findSequence(
+        createCommentReq.getRootCommentId().get()) + 1;
+    return seq;
+  }
+
+//  public Slice<CourseComment> findCourseComment(Long courseId, Pageable pageable) {
+//    Slice<CourseComment> courseComment
+//        = courseCommentRepository.findCourseComment(courseId, pageable);
+//
+//  }
 }
