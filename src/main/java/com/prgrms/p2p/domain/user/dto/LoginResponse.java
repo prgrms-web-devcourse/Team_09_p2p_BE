62dc99d98e1964a222fc792db24d00361b749f78
beomsic <qjatjr29@khu.ac.kr>
2022-08-03 12:17:24 +0900

    [P2P-20] refactor: Login Dto 어노테이션 추가 및 네이밍 변경

--- src/main/java/com/prgrms/p2p/domain/user/dto/LoginResponse.java
+++ src/main/java/com/prgrms/p2p/domain/user/dto/LoginResponse.java
@@ -2,28 +2,34 @@ package com.prgrms.p2p.domain.user.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @Builder
 public class LoginResponse {
 
   private String accessToken;
+  private Datas user;
 
-  public class User {
+  @Getter
+  @NoArgsConstructor
+  public class Datas {
     private Long id;
     private String nickname;
     private String profileImage;
 
-    public User(Long id, String nickname, String profileImage) {
+    public Datas(Long id, String nickname, String profileImage) {
       this.id = id;
       this.nickname = nickname;
       this.profileImage = profileImage;
     }
   }
 
-  public LoginResponse(String accessToken) {
+  public LoginResponse(String accessToken, Datas user) {
     this.accessToken = accessToken;
+    this.user = user;
   }
-
 }
 
