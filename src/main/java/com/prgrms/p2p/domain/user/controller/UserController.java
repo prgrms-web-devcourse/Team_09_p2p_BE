e010182469f05f87f7313d24997e8b6053701632
beomsic <qjatjr29@khu.ac.kr>
2022-08-03 16:26:02 +0900

    [P2P-24] feat: 프로필 이미지 변경 api 추가

--- src/main/java/com/prgrms/p2p/domain/user/controller/UserController.java
+++ src/main/java/com/prgrms/p2p/domain/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.prgrms.p2p.domain.user.controller;
 
+import com.prgrms.p2p.domain.common.service.UploadServiceImpl;
 import com.prgrms.p2p.domain.user.aop.annotation.Auth;
 import com.prgrms.p2p.domain.user.aop.annotation.CurrentUser;
 import com.prgrms.p2p.domain.user.dto.ChangePasswordRequest;
@@ -21,16 +22,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
 
   private final UserService userService;
+  private final UploadServiceImpl uploadService;
 
-  public UserController(UserService userService) {
+  public UserController(UserService userService,
+      UploadServiceImpl uploadService) {
     this.userService = userService;
+    this.uploadService = uploadService;
   }
 
   @PostMapping("/")
@@ -49,13 +55,31 @@ public class UserController {
     return ResponseEntity.ok(login);
   }
 
+  @PostMapping("/email")
+  public void validateEmail(@RequestBody Map<String,String> emailMap) {
+    userService.validateEmail(emailMap.get("email"));
+  }
+
+  @PostMapping("/nickname")
+  public void validateNickname(Map<String,String> nicknameMap) {
+    userService.validateNickname(nicknameMap.get("nickname"));
+  }
+
   @Auth
-  @PutMapping("/")
-  public ResponseEntity modify(@RequestBody ModifyRequest modifyRequest) {
-    userService.modify(modifyRequest.getId(), modifyRequest.getNickname(), modifyRequest.getBirth(),
-        modifyRequest.getSex());
+  @PostMapping("/password")
+  public ResponseEntity changePassword(@RequestBody ChangePasswordRequest changePasswordRequest, @CurrentUser CustomUserDetails user){
+    //TODO: 유저 아이디를 나중에 어노테이션으로 가져올 예정
+    userService.changePassword(user.getId(), changePasswordRequest.getOldPassword(), changePasswordRequest.getNewPassword());
 
     return ResponseEntity.ok().build();
+  }
+
+  @Auth
+  @PostMapping("/profile")
+  public String changeProfileImage(@CurrentUser CustomUserDetails user, @RequestParam() MultipartFile file) {
+    String profileUrl = uploadService.uploadImg(file);
+    userService.changeProfileUrl(user.getId(), profileUrl);
+    return profileUrl;
   }
 
   @Auth
@@ -72,10 +96,10 @@ public class UserController {
   }
 
   @Auth
-  @PostMapping("/password")
-  public ResponseEntity changePassword(@RequestBody ChangePasswordRequest changePasswordRequest, @CurrentUser CustomUserDetails user){
-    //TODO: 유저 아이디를 나중에 어노테이션으로 가져올 예정
-    userService.changePassword(user.getId(), changePasswordRequest.getOldPassword(), changePasswordRequest.getNewPassword());
+  @PutMapping("/")
+  public ResponseEntity modify(@RequestBody ModifyRequest modifyRequest) {
+    userService.modify(modifyRequest.getId(), modifyRequest.getNickname(), modifyRequest.getBirth(),
+        modifyRequest.getSex());
 
     return ResponseEntity.ok().build();
   }
@@ -87,15 +111,5 @@ public class UserController {
     userService.delete(user.getId());
 
     return ResponseEntity.noContent().build();
-  }
-
-  @PostMapping("/email")
-  public void validateEmail(@RequestBody Map<String,String> emailMap) {
-    userService.validateEmail(emailMap.get("email"));
-  }
-
-  @PostMapping("/nickname")
-  public void validateNickname(Map<String,String> nicknameMap) {
-    userService.validateNickname(nicknameMap.get("nickname"));
   }
 }
