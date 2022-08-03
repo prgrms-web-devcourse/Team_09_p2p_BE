24d03d6f33f00c90f6a0c463ecd23418ca1d5a05
Hyunggeun447 <bopi447@naver.com>
2022-08-03 15:41:50 +0900

    [P2P-83] feat: 코스에 대한 댓글을 작성할 수 있습니다.

---
+++ src/main/java/com/prgrms/p2p/domain/comment/dto/CourseCommentResponse.java
@@ -0,0 +1,12 @@
+package com.prgrms.p2p.domain.comment.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class CourseCommentResponse {
+
+
+
+}
--- src/main/java/com/prgrms/p2p/domain/comment/dto/CreateCourseCommentRequest.java
+++ src/main/java/com/prgrms/p2p/domain/comment/dto/CreateCourseCommentRequest.java
@@ -1,13 +1,21 @@
 package com.prgrms.p2p.domain.comment.dto;
 
+import com.fasterxml.jackson.annotation.JsonSetter;
 import java.util.Optional;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class CreateCourseCommentRequest {
 
   private String comment;
   private Optional<Long> rootCommentId;
   private Long userId;
   private Long courseId;
+
+  @JsonSetter
+  public void setRootCommentId(Long rootCommentId) {
+    this.rootCommentId = Optional.ofNullable(rootCommentId);
+  }
 }
