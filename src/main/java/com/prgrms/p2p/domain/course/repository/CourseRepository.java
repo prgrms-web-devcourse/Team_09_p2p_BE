f8979f3cd313acc17b61aaaa4c5c467bc7087cd6
dhkstnaos <dhkstnaos@gmail.com>
2022-08-03 15:53:18 +0900

    feat: 북마크한 코스 조회

--- src/main/java/com/prgrms/p2p/domain/course/repository/CourseRepository.java
+++ src/main/java/com/prgrms/p2p/domain/course/repository/CourseRepository.java
@@ -2,6 +2,8 @@ package com.prgrms.p2p.domain.course.repository;
 
 import com.prgrms.p2p.domain.course.entity.Course;
 import java.util.Optional;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,4 +12,7 @@ public interface CourseRepository extends JpaRepository<Course, Long>, CourseSea
 
   @Query("select c from Course c left join fetch c.coursePlaces")
   Optional<Course> findById(@Param("courseId") Long courseId);
+
+  @Query("select c from Course c left join fetch c.courseBookmarks cb where (:userId is null or cb.userId = :userId)")
+  Slice<Course> findBookmarkedCourse(@Param("userId") Long userId, Pageable pageable);
 }
