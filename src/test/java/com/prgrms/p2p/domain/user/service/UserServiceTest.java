5ecadc41b56eba7daadbd7962999487e0c691f88
unnokid <kimki0609@gmail.com>
2022-08-03 22:44:39 +0900

    [P2P-23] test: getUserInfo 테스트 코드 추가

--- src/test/java/com/prgrms/p2p/domain/user/service/UserServiceTest.java
+++ src/test/java/com/prgrms/p2p/domain/user/service/UserServiceTest.java
@@ -1,12 +1,16 @@
 package com.prgrms.p2p.domain.user.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import com.prgrms.p2p.domain.user.dto.LoginRequest;
 import com.prgrms.p2p.domain.user.dto.SignUpRequest;
+import com.prgrms.p2p.domain.user.dto.UserDetailResponse;
 import com.prgrms.p2p.domain.user.entity.Sex;
-
+import com.prgrms.p2p.domain.user.entity.User;
 import com.prgrms.p2p.domain.user.repository.UserRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,10 +19,10 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
-
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
@@ -139,4 +143,33 @@ class UserServiceTest {
     }
   }
 
+  @Nested
+  @DisplayName("본인 상세정보 조회 테스트")
+  class getUserInfoTest {
+
+    @Test
+    @DisplayName("성공 테스트")
+    void success() {
+      String nickname = userService.signUp(signUpRequest);
+
+      User user = userRepository.findByNickname(nickname).orElseThrow(IllegalArgumentException::new);
+
+      UserDetailResponse userInfo = userService.getUserInfo(user.getId());
+
+      assertThat(userInfo.getId()).isEqualTo(user.getId());
+      assertThat(userInfo.getEmail()).isEqualTo(user.getEmail());
+      assertThat(userInfo.getNickname()).isEqualTo(user.getNickname());
+      assertThat(userInfo.getProfileImage()).isEqualTo(null);
+      assertThat(userInfo.getBirth()).isEqualTo(user.getBirth().toString());
+      assertThat(userInfo.getSex()).isEqualTo(user.getSex());
+    }
+
+    @Test
+    @DisplayName("실패 - 존재하지 않는 Id 입력")
+    void failNotExistId() {
+
+      assertThatThrownBy(() -> userService.getUserInfo(100L))
+          .isInstanceOf(IllegalArgumentException.class);
+    }
+  }
 }
