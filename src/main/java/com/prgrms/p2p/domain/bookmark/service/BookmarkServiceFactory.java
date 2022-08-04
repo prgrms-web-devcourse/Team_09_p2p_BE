0f71e6242e43cf7c66af6255504c8cd187d5fe69
송무송 (Kate) <real.purple.hae.s@gmail.com>
2022-08-04 15:47:24 +0900

    [P2P-157] test: BookmarkServiceFactory 테스트 추가

--- src/main/java/com/prgrms/p2p/domain/bookmark/service/BookmarkServiceFactory.java
+++ src/main/java/com/prgrms/p2p/domain/bookmark/service/BookmarkServiceFactory.java
@@ -1,15 +1,22 @@
 package com.prgrms.p2p.domain.bookmark.service;
 
+import com.prgrms.p2p.domain.common.exception.BadRequestException;
 import java.util.Map;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class BookmarkServiceFactory {
+
   private final Map<String, BookmarkService> bookmarkServiceMap;
 
-  public BookmarkService getBookmarkService(String name){
-    return bookmarkServiceMap.get(name);
+  public BookmarkService getBookmarkService(String name) {
+    BookmarkService bookmarkService = bookmarkServiceMap.get(name);
+    if (Objects.isNull(bookmarkService)) {
+      throw new BadRequestException("잘못된 type 요청입니다.");
+    }
+    return bookmarkService;
   }
 }
