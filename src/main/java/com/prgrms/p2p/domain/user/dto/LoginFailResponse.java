03422979c109d7a04ece9d4ead2e93dae62fb153
beomsic <qjatjr29@khu.ac.kr>
2022-08-05 15:09:59 +0900

    [P2P-99] feat: 로그인 실패했을 경우 Response dto 생성

---
+++ src/main/java/com/prgrms/p2p/domain/user/dto/LoginFailResponse.java
@@ -0,0 +1,18 @@
+package com.prgrms.p2p.domain.user.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Schema(description = "로그인 실패 응답 DTO")
+@Getter
+@NoArgsConstructor
+public class LoginFailResponse {
+
+  @Schema(description = "실패 횟수", example = "4")
+  private Integer count;
+
+  @Schema(description = "제한 시간", example = "2022-08-04T09:51:24")
+  private String expiredAt;
+
+}
