e07de8bcc3f571844ec893a381f385c92b6a3bdf
Hyunggeun447 <bopi447@naver.com>
2022-08-06 00:52:32 +0900

    [P2P-159] test: 댓글 수정 test를 추가합니다.

--- src/test/java/com/prgrms/p2p/domain/comment/service/CourseCommentServiceTest.java
+++ src/test/java/com/prgrms/p2p/domain/comment/service/CourseCommentServiceTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.prgrms.p2p.domain.comment.dto.CreateCommentRequest;
+import com.prgrms.p2p.domain.comment.dto.UpdateCommentRequest;
 import com.prgrms.p2p.domain.comment.entity.CourseComment;
 import com.prgrms.p2p.domain.comment.repository.CourseCommentRepository;
 import com.prgrms.p2p.domain.course.entity.Course;
@@ -239,13 +240,93 @@ class CourseCommentServiceTest {
 
         //when
         Long courseCommentId = courseCommentService.save(createCommentReq, courseId, userId);
-        courseCommentService.deleteCourseComment(courseCommentId,courseId,userId);
+        courseCommentService.deleteCourseComment(courseCommentId, courseId, userId);
         CourseComment courseComment = courseCommentRepository.findById(courseCommentId)
             .orElseThrow(RuntimeException::new);
 
         //then
         assertThat(courseComment.getSeq()).isEqualTo(i);
       }
+    }
+  }
+
+  @Nested
+  @DisplayName("댓글 수정 기능 테스트")
+  class update {
+
+    @Test
+    @DisplayName("성공: 댓글을 정상적으로 수정합니다.")
+    public void updateComment() throws Exception {
+
+      //given
+      String newComment = "new Comment";
+      UpdateCommentRequest updateReq = UpdateCommentRequest.builder()
+          .comment(newComment)
+          .build();
+
+      //when
+      Long changedCommentId = courseCommentService.updateCourseComment(
+          updateReq, rootCommentId1, course.getId(), user.getId());
+
+      //then
+      CourseComment courseComment = courseCommentRepository.findById(changedCommentId)
+          .orElseThrow(RuntimeException::new);
+
+      assertThat(courseComment.getComment()).isEqualTo(newComment);
+    }
+
+    @Test
+    @DisplayName("실패: 유저가 수정 권한이 없습니다.")
+    public void failUpdateCommentAsWrongUser() throws Exception {
+
+      //given
+      String newComment = "new Comment";
+      UpdateCommentRequest updateReq = UpdateCommentRequest.builder()
+          .comment(newComment)
+          .build();
+
+      //when
+      Long userId = -1L;
+
+      //then
+      assertThrows(RuntimeException.class, () -> courseCommentService.updateCourseComment(
+          updateReq, rootCommentId1, course.getId(), userId));
+    }
+
+    @Test
+    @DisplayName("실패: 댓글이 달려있는 게시글이 존재하지 않습니다.")
+    public void failUpdateCommentAsWrongCourse() throws Exception {
+
+      //given
+      String newComment = "new Comment";
+      UpdateCommentRequest updateReq = UpdateCommentRequest.builder()
+          .comment(newComment)
+          .build();
+
+      //when
+      Long courseId = -1L;
+
+      //then
+      assertThrows(RuntimeException.class, () -> courseCommentService.updateCourseComment(
+          updateReq, rootCommentId1, courseId, user.getId()));
+    }
+
+    @Test
+    @DisplayName("실패: 수정하려는 댓글이 존재하지 않습니다.")
+    public void failUpdateCommentAsWrongComment() throws Exception {
+
+      //given
+      String newComment = "new Comment";
+      UpdateCommentRequest updateReq = UpdateCommentRequest.builder()
+          .comment(newComment)
+          .build();
+
+      //when
+      Long commentId = -1L;
+
+      //then
+      assertThrows(RuntimeException.class, () -> courseCommentService.updateCourseComment(
+          updateReq, commentId,  course.getId(), user.getId()));
     }
   }
 }
