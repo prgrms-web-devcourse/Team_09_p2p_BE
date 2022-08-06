91ffe7b101a3bd07301115d7637e9c1e9a242fc6
beomsic <qjatjr29@khu.ac.kr>
2022-08-06 20:40:22 +0900

    [P2P-22] feat: 유저 정보 조회관련 클래스 추가

---
+++ src/main/java/com/prgrms/p2p/domain/user/dto/UserCounts.java
@@ -0,0 +1,28 @@
+package com.prgrms.p2p.domain.user.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Schema(description = "유저의 좋아요, 북마크, 게시물, 댓글 수")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserCounts {
+
+  @Schema(description = "유저가 등록한 코스의 개수", example = "1")
+  private Long courses;
+
+  @Schema(description = "유저가 남긴 댓글 수", example = "1")
+  private UserCommentResponse comments;
+
+  @Schema(description = "유저가 누른 좋아요 수", example = "3")
+  private UserLikeResponse likes;
+
+  @Schema(description = "유저가 즐겨찾기한 수", example = "2")
+  private UserBookmarkResponse bookmarks;
+
+}
