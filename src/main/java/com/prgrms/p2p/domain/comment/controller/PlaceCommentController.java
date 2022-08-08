07f8e99bf9ecb0a0763f0302ad806ac024899d9a
unnokid <kimki0609@gmail.com>
2022-08-08 16:36:50 +0900

    [P2P-87] feat: Controller 장소 댓글 수정 추가

--- src/main/java/com/prgrms/p2p/domain/comment/controller/PlaceCommentController.java
+++ src/main/java/com/prgrms/p2p/domain/comment/controller/PlaceCommentController.java
@@ -1,14 +1,18 @@
 package com.prgrms.p2p.domain.comment.controller;
 
 import com.prgrms.p2p.domain.comment.dto.CreateCommentRequest;
+import com.prgrms.p2p.domain.comment.dto.PlaceCommentResponse;
+import com.prgrms.p2p.domain.comment.dto.UpdateCommentRequest;
 import com.prgrms.p2p.domain.comment.service.PlaceCommentService;
 import com.prgrms.p2p.domain.common.exception.UnAuthorizedException;
 import com.prgrms.p2p.domain.user.aop.annotation.CurrentUser;
 import com.prgrms.p2p.domain.user.pojo.CustomUserDetails;
 import java.util.Objects;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,6 +38,25 @@ public class PlaceCommentController {
     validateLoginUser(user);
     Long commentId = placeCommentService.save(commentRequest, placeId, user.getId());
     return ResponseEntity.ok(commentId);
+  }
+
+  //장소 댓글 수정
+  @PutMapping("/{place_id}/comments/{comment_id}")
+  public ResponseEntity updateComment(
+      @CurrentUser CustomUserDetails user,
+      @RequestBody UpdateCommentRequest updateCommentRequest,
+      @PathVariable("place_id") Long placeId,
+      @PathVariable("comment_id") Long commentId) {
+
+    validateLoginUser(user);
+
+    Long updatedCommentId = placeCommentService.updatePlaceComment(
+        updateCommentRequest,
+        placeId,
+        commentId,
+        user.getId());
+
+    return ResponseEntity.ok(updatedCommentId);
   }
 
   private void validateLoginUser(CustomUserDetails user) {
