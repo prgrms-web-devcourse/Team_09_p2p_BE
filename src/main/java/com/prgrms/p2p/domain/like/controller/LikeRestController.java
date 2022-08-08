621c0f08b7ed3bbddfa0c74559d198a35a9e6203
moosongsong <songe08@gmail.com>
2022-08-08 18:00:58 +0900

    [P2P-130] docs: swagger 북마크/좋아요 API에 적용

--- src/main/java/com/prgrms/p2p/domain/like/controller/LikeRestController.java
+++ src/main/java/com/prgrms/p2p/domain/like/controller/LikeRestController.java
@@ -3,7 +3,12 @@ package com.prgrms.p2p.domain.like.controller;
 import com.prgrms.p2p.domain.like.dto.LikeResponse;
 import com.prgrms.p2p.domain.user.aop.annotation.Auth;
 import com.prgrms.p2p.domain.user.aop.annotation.CurrentUser;
+import com.prgrms.p2p.domain.user.dto.SignUpResponse;
 import com.prgrms.p2p.domain.user.pojo.CustomUserDetails;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,11 +16,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = {"Like API"})
 @RestController
 @RequestMapping("/api/v1/likes")
 @RequiredArgsConstructor
 public class LikeRestController {
 
+  @Operation(summary = "좋아요 토글 기능", description = "로그인 이후 원하는 장소와 코스에 좋아요를 토글합니다.")
+  @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "좋아요 토글 성공", response = SignUpResponse.class),
+      @ApiResponse(code = 401, message = "인증 받지 않은 사용자"),
+      @ApiResponse(code = 404, message = "존재하지 않은 장소/코스로 요청")
+  })
   @Auth
   @GetMapping("/{type}/{id}")
   public ResponseEntity<LikeResponse> toggle(
