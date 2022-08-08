661d039fd75ec9082560f7316faa9b8f72b61147
unnokid <kimki0609@gmail.com>
2022-08-08 16:04:16 +0900

    [P2P-86] feat: 필요한 Dto 추가

---
+++ src/main/java/com/prgrms/p2p/domain/comment/dto/PlaceCommentDto.java
@@ -0,0 +1,31 @@
+package com.prgrms.p2p.domain.comment.dto;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlaceCommentDto {
+
+  private Long id;
+  private String comment;
+  private Long rootCommentId;
+  private LocalDateTime createdAt;
+  private LocalDateTime updatedAt;
+  private UserDto user;
+
+  @Getter
+  @NoArgsConstructor
+  @AllArgsConstructor
+  public static class UserDto {
+
+    private Long id;
+    private String NickName;
+    private String profileImage;
+  }
+}
