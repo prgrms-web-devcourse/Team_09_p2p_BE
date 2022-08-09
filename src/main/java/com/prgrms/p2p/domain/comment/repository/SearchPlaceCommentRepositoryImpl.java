1acbe4212545e5de495e351db0d265841374bc8a
unnokid <kimki0609@gmail.com>
2022-08-09 15:32:30 +0900

    [P2P-93] feat: findPlaceComments 추가

--- src/main/java/com/prgrms/p2p/domain/comment/repository/SearchPlaceCommentRepositoryImpl.java
+++ src/main/java/com/prgrms/p2p/domain/comment/repository/SearchPlaceCommentRepositoryImpl.java
@@ -1,10 +1,13 @@
 package com.prgrms.p2p.domain.comment.repository;
 
 import static com.prgrms.p2p.domain.comment.entity.QPlaceComment.placeComment;
+import static com.prgrms.p2p.domain.user.entity.QUser.*;
 
 import com.prgrms.p2p.domain.comment.dto.PlaceCommentForQueryDsl;
 import com.prgrms.p2p.domain.comment.entity.Visibility;
+import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.NumberExpression;
 import com.querydsl.core.types.dsl.Wildcard;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
@@ -32,7 +35,33 @@ public class SearchPlaceCommentRepositoryImpl implements
 
   @Override
   public List<PlaceCommentForQueryDsl> findPlaceComments(Long placeId) {
+    NumberExpression<Long> otherwise = new CaseBuilder().when(placeComment.rootCommentId.isNull())
+        .then(placeComment.id)
+        .otherwise(placeComment.rootCommentId);
 
-    return null;
+    return jpaQueryFactory.select(
+        Projections.constructor(PlaceCommentForQueryDsl.class,
+            placeComment.id,
+            placeComment.comment,
+            placeComment.rootCommentId,
+            placeComment.createdAt,
+            placeComment.updatedAt,
+            placeComment.visibility,
+            user.id,
+            user.nickname,
+            user.profileUrl
+            )
+        )
+        .from(placeComment)
+        .leftJoin(user).on(placeComment.userId.eq(user.id))
+        .where(
+            placeComment.place.id.eq(placeId),
+            placeComment.visibility.eq(Visibility.TRUE)
+            .or(placeComment.visibility.eq(Visibility.DELETED_INFORMATION))
+        )
+        .orderBy(otherwise.asc())
+        .orderBy(placeComment.createdAt.asc())
+        .fetch();
+
   }
 }
