5eaa363ffd9b4cee9cb9648f73d43d576253a49b
unnokid <kimki0609@gmail.com>
2022-08-09 15:53:47 +0900

    [P2P-93] refactor: @AllArgsConstructor 추가

--- src/main/java/com/prgrms/p2p/domain/comment/dto/PlaceCommentForQueryDsl.java
+++ src/main/java/com/prgrms/p2p/domain/comment/dto/PlaceCommentForQueryDsl.java
@@ -2,11 +2,13 @@ package com.prgrms.p2p.domain.comment.dto;
 
 import com.prgrms.p2p.domain.comment.entity.Visibility;
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class PlaceCommentForQueryDsl {
   private Long id;
   private String comment;
@@ -18,20 +20,6 @@ public class PlaceCommentForQueryDsl {
   private Long userId;
   private String userNickName;
   private String userProfileImage;
-
-  public PlaceCommentForQueryDsl(Long id, String comment, Long rootCommentId,
-      LocalDateTime createdAt, LocalDateTime updatedAt, Visibility visibility,
-      Long userId, String userNickName, String userProfileImage) {
-    this.id = id;
-    this.comment = comment;
-    this.rootCommentId = rootCommentId;
-    this.createdAt = createdAt;
-    this.updatedAt = updatedAt;
-    this.visibility = visibility;
-    this.userId = userId;
-    this.userNickName = userNickName;
-    this.userProfileImage = userProfileImage;
-  }
 
   public void isDeletedComment() {
     this.comment = "삭제된 댓글입니다.";
