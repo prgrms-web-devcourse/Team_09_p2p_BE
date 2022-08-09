f296a541c847f09350814f82c1b676b1c4f27318
unnokid <kimki0609@gmail.com>
2022-08-09 14:24:20 +0900

    [P2P-90] feat: deletePlaceComment 추가

--- src/main/java/com/prgrms/p2p/domain/comment/service/PlaceCommentService.java
+++ src/main/java/com/prgrms/p2p/domain/comment/service/PlaceCommentService.java
@@ -5,6 +5,7 @@ import static com.prgrms.p2p.domain.comment.util.CommentConverter.toPlaceComment
 import com.prgrms.p2p.domain.comment.dto.CreateCommentRequest;
 import com.prgrms.p2p.domain.comment.dto.UpdateCommentRequest;
 import com.prgrms.p2p.domain.comment.entity.PlaceComment;
+import com.prgrms.p2p.domain.comment.entity.Visibility;
 import com.prgrms.p2p.domain.comment.repository.PlaceCommentRepository;
 import com.prgrms.p2p.domain.common.exception.NotFoundException;
 import com.prgrms.p2p.domain.common.exception.UnAuthorizedException;
@@ -15,7 +16,6 @@ import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.validation.annotation.Validated;
 
 @Transactional
 @RequiredArgsConstructor
@@ -28,41 +28,60 @@ public class PlaceCommentService {
 
 
   public Long save(CreateCommentRequest createCommentRequest, Long placeId, Long userId) {
-    //유저가 존재하는지 확인
     validateAuth(!userRepository.existsById(userId), "존재하지 않는 계정입니다.");
 
-    //Place 가져오기
     Place place = placeRepository.findById(placeId)
         .orElseThrow(() -> new NotFoundException("장소가 존재하지 않습니다."));
 
-    //상위 댓글이 있는지 확인
     Long rootCommentId = createCommentRequest.getRootCommentId();
     validateAuth(
         !Objects.isNull(rootCommentId) && !placeCommentRepository.existsById(rootCommentId),
         "존재하지 않는 댓글에 하위 댓글을 작성할 수 없습니다.");
 
-    //placeComment으로 바꾸기
     PlaceComment placeComment = toPlaceComment(createCommentRequest, place, userId);
-
     return placeCommentRepository.save(placeComment).getId();
   }
 
-  public Long updatePlaceComment(UpdateCommentRequest updateCommentRequest, Long placeId, Long commentId, Long userId) {
-
-    //장소 실제 여부
+  public Long updatePlaceComment(UpdateCommentRequest updateCommentRequest, Long placeId, Long placeCommentId, Long userId) {
     validateAuth(!placeRepository.existsById(placeId),"존재하지 않는 장소 입니다.");
 
-    //placeComment 가져오기
-    PlaceComment placeComment = placeCommentRepository.findById(commentId)
+    PlaceComment placeComment = placeCommentRepository.findById(placeCommentId)
         .orElseThrow(() -> new NotFoundException("존재하지 않는 댓글입니다."));
 
-    //수정 권한 확인
     validateAuth(!placeComment.getUserId().equals(userId), "댓글의 수정 권한이 없습니다.");
 
-    //Comment 바꾸어주기
     placeComment.changeComment(updateCommentRequest.getComment());
-
     return placeComment.getId();
+  }
+
+  public void deletePlaceComment(Long placeId, Long placeCommentId, Long userId){
+    validateAuth(!placeRepository.existsById(placeId),"존재하지 않는 장소 입니다.");
+
+    PlaceComment placeComment = placeCommentRepository.findById(placeCommentId)
+        .orElseThrow(() -> new NotFoundException("존재하지 않는 댓글입니다."));
+
+    validateAuth(!placeComment.getUserId().equals(userId), "댓글의 수정 권한이 없습니다.");
+
+    if(!Objects.isNull(placeComment.getRootCommentId())){
+      checkDeleteParentComment(placeComment);
+      placeComment.changeVisibility(Visibility.FALSE);
+      return;
+    }
+
+    if(placeCommentRepository.findSubCommentCount(placeComment.getRootCommentId()) == 0){
+      placeComment.changeVisibility(Visibility.FALSE);
+      return;
+    }
+
+    placeComment.changeVisibility(Visibility.DELETED_INFORMATION);
+  }
+
+  private void checkDeleteParentComment(PlaceComment placeComment){
+    if(placeCommentRepository.findSubCommentCount(placeComment.getRootCommentId()) == 1){
+      PlaceComment parentComment = placeCommentRepository.findById(placeComment.getRootCommentId())
+          .orElseThrow(() -> new NotFoundException("부모 댓글이 존재하지 않습니다."));
+      parentComment.changeVisibility(Visibility.FALSE);
+    }
   }
 
   private void validateAuth(boolean courseComment, String message) {
