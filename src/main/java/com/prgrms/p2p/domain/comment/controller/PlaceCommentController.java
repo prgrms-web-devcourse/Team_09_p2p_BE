0723567fc140e63aa6c6d17d9e7501495d2bc517
unnokid <kimki0609@gmail.com>
2022-08-09 15:01:14 +0900

    [P2P-93] feat: getCommentList 추가

--- src/main/java/com/prgrms/p2p/domain/comment/controller/PlaceCommentController.java
+++ src/main/java/com/prgrms/p2p/domain/comment/controller/PlaceCommentController.java
@@ -1,14 +1,17 @@
 package com.prgrms.p2p.domain.comment.controller;
 
 import com.prgrms.p2p.domain.comment.dto.CreateCommentRequest;
+import com.prgrms.p2p.domain.comment.dto.PlaceCommentResponse;
 import com.prgrms.p2p.domain.comment.dto.UpdateCommentRequest;
 import com.prgrms.p2p.domain.comment.service.PlaceCommentService;
+import com.prgrms.p2p.domain.comment.service.SearchPlaceCommentService;
 import com.prgrms.p2p.domain.common.exception.UnAuthorizedException;
 import com.prgrms.p2p.domain.user.aop.annotation.CurrentUser;
 import com.prgrms.p2p.domain.user.pojo.CustomUserDetails;
 import java.util.Objects;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -21,10 +24,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class PlaceCommentController {
 
   private PlaceCommentService placeCommentService;
+  private SearchPlaceCommentService searchPlaceCommentService;
 
   public PlaceCommentController(
-      PlaceCommentService placeCommentService) {
+      PlaceCommentService placeCommentService,
+      SearchPlaceCommentService searchPlaceCommentService) {
     this.placeCommentService = placeCommentService;
+    this.searchPlaceCommentService = searchPlaceCommentService;
   }
 
   //장소 댓글 등록
@@ -75,4 +81,14 @@ public class PlaceCommentController {
       throw new UnAuthorizedException("로그인이 필요합니다.");
     }
   }
+
+  //장소 댓글 전체 조회
+  @GetMapping("/{place_id}/comments")
+  public ResponseEntity<PlaceCommentResponse> getCommentList(@PathVariable("place_id") Long placeId) {
+
+    PlaceCommentResponse placeComment = searchPlaceCommentService.findPlaceComment(placeId);
+
+    return ResponseEntity.ok(placeComment);
+  }
+
 }
