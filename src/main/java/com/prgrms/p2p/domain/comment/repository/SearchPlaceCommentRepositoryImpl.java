077e2e6e44fa2861cff64e23bf34795b968d1397
unnokid <kimki0609@gmail.com>
2022-08-09 14:44:19 +0900

    [P2P-90] feat: findSubCommentCount 수정

--- src/main/java/com/prgrms/p2p/domain/comment/repository/SearchPlaceCommentRepositoryImpl.java
+++ src/main/java/com/prgrms/p2p/domain/comment/repository/SearchPlaceCommentRepositoryImpl.java
@@ -1,28 +1,38 @@
 package com.prgrms.p2p.domain.comment.repository;
 
-import static com.prgrms.p2p.domain.comment.entity.QCourseComment.courseComment;
+import static com.prgrms.p2p.domain.comment.entity.QPlaceComment.placeComment;
 
+import com.prgrms.p2p.domain.comment.dto.PlaceCommentForQueryDsl;
 import com.prgrms.p2p.domain.comment.entity.Visibility;
+import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.core.types.dsl.Wildcard;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
+import javax.persistence.EntityManager;
 
 public class SearchPlaceCommentRepositoryImpl implements
     SearchPlaceCommentRepository {
 
   private final JPAQueryFactory jpaQueryFactory;
 
-  public SearchPlaceCommentRepositoryImpl(JPAQueryFactory jpaQueryFactory) {
-    this.jpaQueryFactory = jpaQueryFactory;
+  public SearchPlaceCommentRepositoryImpl(EntityManager em) {
+    this.jpaQueryFactory = new JPAQueryFactory(em);
   }
 
   @Override
   public Long findSubCommentCount(Long commentId) {
     return jpaQueryFactory.select(Wildcard.count)
-        .from(courseComment)
-        .where(courseComment.rootCommentId.eq(commentId),
-            courseComment.visibility.eq(Visibility.TRUE)
+        .from(placeComment)
+        .where(placeComment.rootCommentId.eq(commentId),
+            placeComment.visibility.eq(Visibility.TRUE)
         )
         .fetch()
         .get(0);
+  }
+
+  @Override
+  public List<PlaceCommentForQueryDsl> findPlaceComments(Long placeId) {
+
+    return null;
   }
 }
