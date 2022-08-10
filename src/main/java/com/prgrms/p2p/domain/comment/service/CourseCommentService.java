c5c12440909560f1f423f082f3430a667747c417
Hyunggeun447 <bopi447@naver.com>
2022-08-10 18:33:53 +0900

    [P2P-177] feat: 코스 댓글 예외 관리 및 스웨거

--- src/main/java/com/prgrms/p2p/domain/comment/service/CourseCommentService.java
+++ src/main/java/com/prgrms/p2p/domain/comment/service/CourseCommentService.java
@@ -1,5 +1,6 @@
 package com.prgrms.p2p.domain.comment.service;
 
+import static com.prgrms.p2p.domain.comment.entity.Visibility.*;
 import static com.prgrms.p2p.domain.comment.entity.Visibility.DELETED_INFORMATION;
 import static com.prgrms.p2p.domain.comment.entity.Visibility.FALSE;
 import static com.prgrms.p2p.domain.comment.util.CommentConverter.toCourseComment;
@@ -30,7 +31,7 @@ public class CourseCommentService {
 
   public Long save(CreateCommentRequest createCommentReq, Long courseId, Long userId) {
 
-    validateAuth(!userRepository.existsById(userId), "존재하지 않는 계정입니다.");
+    notFoundMessage(!userRepository.existsById(userId), "존재하지 않는 계정입니다.");
     Course course = getCourse(courseId);
 
     Long rootCommentId = createCommentReq.getRootCommentId();
@@ -51,9 +52,10 @@ public class CourseCommentService {
   public Long updateCourseComment(UpdateCommentRequest updateReq, Long courseCommentId,
       Long courseId, Long userId) {
 
-    validateAuth(!courseRepository.existsById(courseId), "게시글이 존재하지 않습니다.");
+    notFoundMessage(!courseRepository.existsById(courseId), "게시글이 존재하지 않습니다.");
     CourseComment courseComment = courseCommentRepository.findById(courseCommentId)
         .orElseThrow(() -> new NotFoundException("수정 불가: 존재하지 않은 댓글입니다"));
+
     validateAuth(!courseComment.getUserId().equals(userId), "댓글의 수정 권한이 없습니다.");
 
     courseComment.changeComment(updateReq.getComment());
@@ -62,9 +64,13 @@ public class CourseCommentService {
 
   public void deleteCourseComment(Long courseCommentId, Long courseId, Long userId) {
 
-    validateAuth(!courseRepository.existsById(courseId), "게시글이 존재하지 않습니다.");
+    notFoundMessage(!courseRepository.existsById(courseId), "게시글이 존재하지 않습니다.");
     CourseComment courseComment = courseCommentRepository.findById(courseCommentId)
         .orElseThrow(() -> new NotFoundException("삭제 불가: 존재하지 않은 댓글입니다"));
+
+    if (!courseComment.getVisibility().equals(TRUE)) {
+      throw new BadRequestException("이미 삭제된 댓글은 삭제할 수 없습니다.");
+    }
     validateAuth(!courseComment.getUserId().equals(userId), "댓글의 삭제 권한이 없습니다.");
 
     if (!Objects.isNull(courseComment.getRootCommentId())) {
@@ -83,8 +89,14 @@ public class CourseCommentService {
 
   private Course getCourse(Long courseId) {
     Course course = courseRepository.findById(courseId)
-        .orElseThrow(() -> new NotFoundException("게시글이 존재하지 않습니다. courseId:" + courseId));
+        .orElseThrow(() -> new NotFoundException("게시글이 존재하지 않습니다."));
     return course;
+  }
+
+  private void notFoundMessage(boolean courseComment, String message) {
+    if (courseComment) {
+      throw new NotFoundException(message);
+    }
   }
 
   private void validateAuth(boolean courseComment, String message) {
