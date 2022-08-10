84a2b6acada0631499867babfe979d963d851703
unnokid <kimki0609@gmail.com>
2022-08-10 18:18:05 +0900

    [P2P-118] test: 테스트할 댓글 초기화

--- src/test/java/com/prgrms/p2p/domain/comment/service/SearchPlaceCommentServiceTest.java
+++ src/test/java/com/prgrms/p2p/domain/comment/service/SearchPlaceCommentServiceTest.java
@@ -1,13 +1,7 @@
 package com.prgrms.p2p.domain.comment.service;
 
-import com.prgrms.p2p.domain.comment.entity.CourseComment;
 import com.prgrms.p2p.domain.comment.entity.PlaceComment;
 import com.prgrms.p2p.domain.comment.repository.PlaceCommentRepository;
-import com.prgrms.p2p.domain.course.entity.Course;
-import com.prgrms.p2p.domain.course.entity.Period;
-import com.prgrms.p2p.domain.course.entity.Region;
-import com.prgrms.p2p.domain.course.entity.Spot;
-import com.prgrms.p2p.domain.course.entity.Theme;
 import com.prgrms.p2p.domain.place.entity.Address;
 import com.prgrms.p2p.domain.place.entity.Category;
 import com.prgrms.p2p.domain.place.entity.PhoneNumber;
@@ -17,7 +11,6 @@ import com.prgrms.p2p.domain.user.entity.Sex;
 import com.prgrms.p2p.domain.user.entity.User;
 import com.prgrms.p2p.domain.user.repository.UserRepository;
 import java.time.LocalTime;
-import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -65,15 +58,6 @@ public class SearchPlaceCommentServiceTest {
     Category category = Category.MT1;
     PhoneNumber phoneNumber = new PhoneNumber("010-2345-5678");
 
-    //유저
-    String email = "qjatlr@gmail.com";
-    String password = "root1234!";
-    String nick = "nickname";
-    String birth = "2010-01-01";
-    Sex male = Sex.FEMALE;
-    user = userRepository.save(new User(email, password, nick, birth, male));
-    Long userId = user.getId();
-
     place = placeRepository.save(
         new Place(
             kakaoMapId,
@@ -86,14 +70,23 @@ public class SearchPlaceCommentServiceTest {
         )
     );
 
+    //유저
+    String email = "qjatlr@gmail.com";
+    String password = "root1234!";
+    String nickname = "nickname";
+    String birth = "2010-01-01";
+    Sex male = Sex.FEMALE;
+    user = userRepository.save(new User(email, password, nickname, birth, male));
+    Long userId = user.getId();
+
     // parent comment 1
     parentComment1 = placeCommentRepository.save(
         new PlaceComment("parentComment1.                 작성시간 : " + LocalTime.now()
-            , null, user.getId(), place));
+            , null, userId, place));
 
     placeCommentRepository.save(
         new PlaceComment("------> 1번의 첫번째 sub 댓글이에요. 작성시간 : " + LocalTime.now(),
-            parentComment1.getId(), user.getId(), place));
+            parentComment1.getId(), userId, place));
 
     // parent comment 2
     parentComment2 = placeCommentRepository.save(
@@ -102,34 +95,34 @@ public class SearchPlaceCommentServiceTest {
 
     middleSubComment = placeCommentRepository.save(
         new PlaceComment("------> 1번의 두번째 sub 댓글이에요. 작성시간 : " + LocalTime.now(),
-            parentComment1.getId(), user.getId(), place));
+            parentComment1.getId(), userId, place));
     placeCommentRepository.save(
         new PlaceComment("------> 2번의 첫번째 sub 댓글이에요. 작성시간 : " + LocalTime.now(),
-            parentComment2.getId(), user.getId(), place));
+            parentComment2.getId(), userId, place));
 
     placeCommentRepository.save(
         new PlaceComment("------> 2번의 두번째 sub 댓글이에요. 작성시간 : " + LocalTime.now(),
-            parentComment2.getId(), user.getId(), place));
+            parentComment2.getId(), userId, place));
     placeCommentRepository.save(
         new PlaceComment("------> 2번의 세번째 sub 댓글이에요. 작성시간 : " + LocalTime.now(),
-            parentComment2.getId(), user.getId(), place));
+            parentComment2.getId(), userId, place));
     placeCommentRepository.save(
         new PlaceComment("------> 1번의 세번째 sub 댓글이에요. 작성시간 : " + LocalTime.now(),
-            parentComment1.getId(), user.getId(), place));
+            parentComment1.getId(), userId, place));
 
     // parent comment 3
     parentComment3 = placeCommentRepository.save(
         new PlaceComment("parentComment3.                 작성시간 : " + LocalTime.now(),
-            null, user.getId(), place));
+            null, userId, place));
 
     // parent comment 4
     parentComment4 = placeCommentRepository.save(
         new PlaceComment("parentComment4.                 작성시간 : " + LocalTime.now(),
-            null, user.getId(), place));
+            null, userId, place));
 
     lastSubComment = placeCommentRepository.save(
         new PlaceComment("------> 4번의 첫번째 sub 댓글이에요. 작성시간 : " + LocalTime.now(),
-            parentComment4.getId(), user.getId(), place));
+            parentComment4.getId(), userId, place));
   }
 
 
