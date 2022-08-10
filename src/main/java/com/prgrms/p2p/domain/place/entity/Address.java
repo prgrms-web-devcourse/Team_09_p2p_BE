5622a2e6b5795b4ae53b75730ad72f599bbe245e
Hyunggeun447 <bopi447@naver.com>
2022-08-10 17:04:01 +0900

    [P2P-185] feat: @PageableDefault 추가

--- src/main/java/com/prgrms/p2p/domain/place/entity/Address.java
+++ src/main/java/com/prgrms/p2p/domain/place/entity/Address.java
@@ -1,5 +1,7 @@
 package com.prgrms.p2p.domain.place.entity;
 
+import com.prgrms.p2p.domain.common.exception.BadRequestException;
+import java.util.Objects;
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Embeddable;
@@ -19,8 +21,8 @@ public class Address {
   private String roadAddressName;
 
   public Address(String addressName, String roadAddressName) {
-    if (addressName == null && roadAddressName == null) {
-      throw new IllegalArgumentException("2가지 주소 중 최소한 하나는 있어야 함");
+    if (Objects.isNull(addressName) && Objects.isNull(roadAddressName)) {
+      throw new BadRequestException("2가지 주소 중 최소한 하나는 있어야 함");
     }
     this.addressName = addressName;
     this.roadAddressName = roadAddressName;
