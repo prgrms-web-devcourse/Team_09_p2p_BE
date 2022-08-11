edb39312ec895382f33f7e2c65b8e16e5b976390
dhkstnaos <dhkstnaos@gmail.com>
2022-08-11 22:09:49 +0900

    fix: 목록 조회에 spots 추가

--- src/test/java/com/prgrms/p2p/domain/course/service/CourseQueryServiceTest.java
+++ src/test/java/com/prgrms/p2p/domain/course/service/CourseQueryServiceTest.java
@@ -1,5 +1,6 @@
 package com.prgrms.p2p.domain.course.service;
 
+import static com.prgrms.p2p.domain.course.util.CourseConverter.ofSummary;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
@@ -130,25 +131,6 @@ class CourseQueryServiceTest {
       Slice<SummaryCourseResponse> summaryList = courseQueryService.findBookmarkedCourseList(any(),
           any(Long.class));
       verify(courseRepository, times(1)).findBookmarkedCourse(any(Long.class), any());
-
-      Assertions.assertThat(summaryList.getContent().size()).isEqualTo(1);
-      Assertions.assertThatObject(summaryList.getContent().get(0).getClass())
-          .isEqualTo(SummaryCourseResponse.class);
-      Assertions.assertThat(summaryList.getContent().get(0).getIsBookmarked()).isTrue();
-    }
-  }
-
-  @Nested
-  @DisplayName("유저가 등록한 코스 조회")
-  class findMyCourseList {
-
-    @Test
-    @DisplayName("성공: 북마크한 코스 조회")
-    void successFindMyCourseList() {
-      when(courseRepository.findByUser_IdOrderByCreatedAtDesc(any(Long.class), any())).thenReturn(courses);
-      Slice<SummaryCourseResponse> summaryList = courseQueryService.findMyCourseList(any(),
-          any(Long.class));
-      verify(courseRepository, times(1)).findByUser_IdOrderByCreatedAtDesc(any(Long.class), any());
 
       Assertions.assertThat(summaryList.getContent().size()).isEqualTo(1);
       Assertions.assertThatObject(summaryList.getContent().get(0).getClass())
