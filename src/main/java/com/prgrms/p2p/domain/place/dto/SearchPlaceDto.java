c3fca0990633f8eeb53ca35f66ef4e82309ba4ad
Hyunggeun447 <bopi447@naver.com>
2022-08-11 17:17:37 +0900

    [P2P-192] feat: 장소 요청, 응답 dto 스웨거 추가

--- src/main/java/com/prgrms/p2p/domain/place/dto/SearchPlaceDto.java
+++ src/main/java/com/prgrms/p2p/domain/place/dto/SearchPlaceDto.java
@@ -2,6 +2,7 @@ package com.prgrms.p2p.domain.place.dto;
 
 import com.prgrms.p2p.domain.course.entity.Region;
 import com.prgrms.p2p.domain.course.entity.Sorting;
+import io.swagger.annotations.ApiParam;
 import java.util.Optional;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,7 +15,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class SearchPlaceDto {
 
+  @ApiParam(value = "장소 검색어", example = "횟집")
   private Optional<String> keyword;
+
+  @ApiParam(value = "지역 검색 조건", example = "인천")
   private Optional<Region> region;
+
+  @ApiParam(value = "장소 검색 정렬 순서", example = "최신순")
   private Optional<Sorting> sorting;
 }
