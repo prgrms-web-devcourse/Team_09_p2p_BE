dd0e47e225b678d34bdea7f25b1a55e3489e10e0
dhkstnaos <dhkstnaos@gmail.com>
2022-08-12 16:38:11 +0900

    fix : 지워진 코스 장소는 안 보이게 수정

--- src/main/java/com/prgrms/p2p/domain/course/repository/CoursePlaceRepository.java
+++ src/main/java/com/prgrms/p2p/domain/course/repository/CoursePlaceRepository.java
@@ -1,8 +1,11 @@
 package com.prgrms.p2p.domain.course.repository;
 
+import com.prgrms.p2p.domain.course.entity.Course;
 import com.prgrms.p2p.domain.course.entity.CoursePlace;
+import com.prgrms.p2p.domain.place.entity.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CoursePlaceRepository extends JpaRepository<CoursePlace, Long> {
 
+  void deleteBySeqAndCourseAndPlace(int seq, Course course, Place place);
 }
