e7e3a896b5f48153d0bca5663d18ec824c0c9e0f
unnokid <kimki0609@gmail.com>
2022-08-12 17:34:40 +0900

    [P2P-118] test: 장소의 댓글조회 기능 테스트 추가

--- src/test/java/com/prgrms/p2p/domain/comment/service/SearchPlaceCommentServiceTest.java
+++ src/test/java/com/prgrms/p2p/domain/comment/service/SearchPlaceCommentServiceTest.java
@@ -1,5 +1,9 @@
 package com.prgrms.p2p.domain.comment.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.prgrms.p2p.domain.comment.dto.PlaceCommentDto;
+import com.prgrms.p2p.domain.comment.dto.PlaceCommentResponse;
 import com.prgrms.p2p.domain.comment.entity.PlaceComment;
 import com.prgrms.p2p.domain.comment.repository.PlaceCommentRepository;
 import com.prgrms.p2p.domain.place.entity.Address;
@@ -11,7 +15,12 @@ import com.prgrms.p2p.domain.user.entity.Sex;
 import com.prgrms.p2p.domain.user.entity.User;
 import com.prgrms.p2p.domain.user.repository.UserRepository;
 import java.time.LocalTime;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
@@ -125,5 +134,126 @@ public class SearchPlaceCommentServiceTest {
             parentComment4.getId(), userId, place));
   }
 
+  @Test
+  @DisplayName("순서에 맞게 댓글 전체 가져오기")
+  public void getCorrectSequence() {
 
+    // when
+    PlaceCommentResponse response = searchPlaceCommentService.findPlaceComment(place.getId());
+
+    for (int i = 0; i < response.getPlaceComments().size() - 1; i++) {
+
+      if (Objects.isNull(response.getPlaceComments().get(i).getRootCommentId())) {
+
+        if (Objects.isNull(response.getPlaceComments().get(i + 1).getRootCommentId())) {
+          assertThat(response.getPlaceComments().get(i).getCreatedAt())
+              .isBefore(response.getPlaceComments().get(i + 1).getCreatedAt());
+
+        } else {
+          assertThat(response.getPlaceComments().get(i).getId())
+              .isEqualTo(response.getPlaceComments().get(i + 1).getRootCommentId());
+          assertThat(response.getPlaceComments().get(i).getCreatedAt())
+              .isBefore(response.getPlaceComments().get(i + 1).getCreatedAt());
+        }
+
+      } else {
+        if (!Objects.isNull(response.getPlaceComments().get(i + 1).getRootCommentId())) {
+          assertThat(response.getPlaceComments().get(i).getRootCommentId())
+              .isEqualTo(response.getPlaceComments().get(i + 1).getRootCommentId());
+          assertThat(response.getPlaceComments().get(i).getCreatedAt())
+              .isBefore(response.getPlaceComments().get(i + 1).getCreatedAt());
+        }
+      }
+    }
+  }
+
+  @Test
+  @DisplayName("대댓글이 없는 댓글을 삭제하면 표기되지 않습니다.")
+  public void deleteParentCommentNotExistSubComment() {
+
+    PlaceCommentResponse responseBefore = searchPlaceCommentService.findPlaceComment(place.getId());
+    List<PlaceCommentDto> beforeList = responseBefore.getPlaceComments().stream()
+        .filter(bl -> bl.getId().equals(parentComment3.getId()))
+        .collect(Collectors.toList());
+
+    assertThat(beforeList.size()).isEqualTo(1);
+
+    placeCommentService.deletePlaceComment(place.getId(),parentComment3.getId(), user.getId());
+
+    PlaceCommentResponse responseAfter = searchPlaceCommentService.findPlaceComment(place.getId());
+    List<PlaceCommentDto> afterList = responseAfter.getPlaceComments().stream()
+        .filter(al -> al.getId().equals(parentComment3.getId()))
+        .collect(Collectors.toList());
+
+    assertThat(afterList.size()).isEqualTo(0);
+  }
+
+  @Test
+  @DisplayName("대댓글이 있는 댓글을 칸은 남아 있지만 자세히 표기되지 않습니다.")
+  public void deleteParentCommentExistSubComment() {
+
+    PlaceCommentResponse responseBefore = searchPlaceCommentService.findPlaceComment(place.getId());
+    List<PlaceCommentDto> beforeList = responseBefore.getPlaceComments().stream()
+        .filter(bl -> bl.getId().equals(parentComment4.getId()))
+        .collect(Collectors.toList());
+
+    assertThat(beforeList.size()).isEqualTo(1);
+
+    placeCommentService.deletePlaceComment(place.getId(), parentComment4.getId(), user.getId());
+
+    PlaceCommentResponse responseAfter = searchPlaceCommentService.findPlaceComment(place.getId());
+    List<PlaceCommentDto> afterList = responseAfter.getPlaceComments().stream()
+        .filter(al -> al.getId().equals(parentComment4.getId()))
+        .collect(Collectors.toList());
+
+    assertThat(afterList.size()).isEqualTo(1);
+    assertThat(afterList.get(0).getComment()).isEqualTo("삭제된 댓글입니다.");
+    assertThat(afterList.get(0).getUser().getNickName()).isNull();
+    assertThat(afterList.get(0).getUser().getProfileImage()).isNull();
+    assertThat(afterList.get(0).getCreatedAt()).isNull();
+    assertThat(afterList.get(0).getUpdatedAt()).isNull();
+  }
+
+  @Test
+  @DisplayName("대댓글을 삭제할때 부모를 포함한 모두가 삭제된 상태라면 표기되지 않습니다.")
+  public void deleteSubCommentNoOneNotExist() {
+
+    PlaceCommentResponse responseBefore = searchPlaceCommentService.findPlaceComment(place.getId());
+    List<PlaceCommentDto> beforeList = responseBefore.getPlaceComments().stream()
+        .filter(bl -> bl.getId().equals(parentComment4.getId()))
+        .collect(Collectors.toList());
+
+    assertThat(beforeList.size()).isEqualTo(1);
+
+    placeCommentService.deletePlaceComment(place.getId(), parentComment4.getId(), user.getId());
+    placeCommentService.deletePlaceComment(place.getId(), lastSubComment.getId(), user.getId());
+
+    PlaceCommentResponse responseAfter = searchPlaceCommentService.findPlaceComment(place.getId());
+    List<PlaceCommentDto> afterList = responseAfter.getPlaceComments().stream()
+        .filter(al -> al.getId().equals(parentComment4.getId()))
+        .collect(Collectors.toList());
+
+    assertThat(afterList.size()).isEqualTo(0);
+  }
+
+  @Test
+  @DisplayName("대댓글은 바로 삭제할 수 있습니다.")
+  public void deleteSubComment() {
+
+    PlaceCommentResponse responseBefore = searchPlaceCommentService.findPlaceComment(place.getId());
+    List<PlaceCommentDto> beforeList = responseBefore.getPlaceComments().stream()
+        .filter(bl -> bl.getId().equals(middleSubComment.getId()))
+        .collect(Collectors.toList());
+
+    assertThat(beforeList.size()).isEqualTo(1);
+
+    placeCommentService.deletePlaceComment(place.getId(), middleSubComment.getId(), user.getId());
+
+    PlaceCommentResponse responseAfter = searchPlaceCommentService.findPlaceComment(place.getId());
+    List<PlaceCommentDto> afterList = responseAfter.getPlaceComments().stream()
+        .filter(al -> al.getId().equals(middleSubComment.getId()))
+        .collect(Collectors.toList());
+
+    assertThat(afterList.size()).isEqualTo(0);
+  }
 }
