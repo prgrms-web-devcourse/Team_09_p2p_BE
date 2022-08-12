fb3584f6771ce8030ddce80813f2ab9939042b51
beomsic <qjatjr29@khu.ac.kr>
2022-08-13 00:33:57 +0900

    [P2P-22] feat: 토큰 변조, 만료시 필터에서 예외처리(status: 401)

--- src/main/java/com/prgrms/p2p/domain/user/config/security/JwtAuthenticationFilter.java
+++ src/main/java/com/prgrms/p2p/domain/user/config/security/JwtAuthenticationFilter.java
@@ -3,6 +3,9 @@ package com.prgrms.p2p.domain.user.config.security;
 import com.prgrms.p2p.domain.user.pojo.CustomUserDetails;
 import com.prgrms.p2p.domain.user.repository.LogoutTokenRedisRepository;
 import com.prgrms.p2p.domain.user.service.CustomUserDetailService;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureException;
 import java.io.IOException;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -10,6 +13,9 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -19,6 +25,7 @@ import org.springframework.web.filter.GenericFilterBean;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends GenericFilterBean {
 
+  private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
   private final JwtTokenProvider jwtTokenProvider;
   private final LogoutTokenRedisRepository logoutTokenRedisRepository;
   private final CustomUserDetailService customUserDetailService;
@@ -27,18 +34,24 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
       throws IOException, ServletException {
 
-    String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
+    try {
+      String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
 
-    if(token != null){
+      if(token != null){
 
-      String userEmail = jwtTokenProvider.getUserEmail(token);
-      if(userEmail != null) {
-        CustomUserDetails userDetails = customUserDetailService.loadUserByUsername(userEmail);
+        String userEmail = jwtTokenProvider.getUserEmail(token);
+        if(userEmail != null) {
+          CustomUserDetails userDetails = customUserDetailService.loadUserByUsername(userEmail);
 
-        validateAccessToken(token, userDetails);
-        Authentication authentication = jwtTokenProvider.getAuthentication(userDetails);
-        SecurityContextHolder.getContext().setAuthentication(authentication);
+          validateAccessToken(token, userDetails);
+          Authentication authentication = jwtTokenProvider.getAuthentication(userDetails);
+          SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
       }
+    } catch (SignatureException | MalformedJwtException e) {
+      logger.info("토큰이 변조되었습니다.");
+    } catch (ExpiredJwtException e) {
+      logger.info("토큰이 만료되었습니다.");
     }
     chain.doFilter(request,response);
   }
