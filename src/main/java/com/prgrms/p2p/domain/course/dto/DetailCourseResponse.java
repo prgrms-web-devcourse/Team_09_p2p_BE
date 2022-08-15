a95dc06e852e50c90835979b37fd4e77ee7ff211
dhkstnaos <dhkstnaos@gmail.com>
2022-08-15 17:56:13 +0900

    feat: 코스 상세 조회에 댓글 개수 추가

--- src/main/java/com/prgrms/p2p/domain/course/dto/DetailCourseResponse.java
+++ src/main/java/com/prgrms/p2p/domain/course/dto/DetailCourseResponse.java
@@ -30,6 +30,7 @@ public class DetailCourseResponse {
   @Builder.Default
   private List<CoursePlaceResponse> places = new ArrayList<>();
   private Integer likes;
+  private Integer comments;
   private Boolean isLiked;
   private Boolean isBookmarked;
   private Long userId;
